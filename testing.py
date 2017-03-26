5e986c9e95f2127d4fb58edd858794dda40556e2
2020amittal <angeli.mittal@gmail.com>
2017-03-26 06:16:26 -0400

    asd

--- testing.py
+++ testing.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+from google import search
+from bs4 import BeautifulSoup
+import urllib
 
 app = Flask(__name__)
+
+def google_scrape(url):
+    thepage = urllib.urlopen(url)
+    soup = BeautifulSoup(thepage, "html.parser")
+    return soup.title.text
+
+def receive():
+    i = 1
+    query = "search this"
+    for url in search(query, stop=10):
+        a = google_scrape(url)
+        print str(i) + ". " + a
+        print urlprint " "
+        print " "
+        i += 1
+
+@app.route("/submit", methods = ["POST"])
+def submit():
+    return "Hello World!"
+    #return render_template("inputform.html")
+
 
 @app.route("/")
 def home():
   return render_template("index.html")
+
 
 @app.route("/")
 def contact():
@@ -13,6 +38,8 @@ def contact():
 @app.route("/")
 def inputform():
   return render_template("inputform.html")
+
+
 
 if __name__ == "__main__":
   app.run()
