d6033a14392790ea1d22824d391b1fa779fdc698
MDass <MD@USComputers.net>
2017-03-26 11:38:37 -0400

    filters

---
+++ datafilter.py
@@ -0,0 +1,23 @@
+url = "www.google.com/video"
+string = "video"
+
+dictionary = {} # may be [] or ()
+if string in url:
+  key =url
+  value = string
+  dictionary[key] = value
+
+url = "www.google.com/image"
+string = "image"
+
+dictionary = {} # may be [] or ()
+if string in url:
+  key =url
+  value = string
+  dictionary[key] = value
+##############
+
+if #academic is selected:
+    searchString = string + " .edu .gov .org"
+else: #causual is selected
+    searchString = string
